72bbc3a081db534a70df19935a367b24df26ee9f
Ben Forrest <ben@silvabox.com>
2016-10-11 21:05:30 +0100

    adds script to check booking engine status

--- Gemfile
+++ Gemfile
@@ -6,4 +6,6 @@ ruby '2.3.0'
 source "https://rubygems.org"
 
 gem 'capybara'
+gem 'capybara-webkit'
 gem 'dotenv'
+gem 'orchestrate'
--- lib/script.rb
+++ lib/script.rb
@@ -1,15 +1,53 @@
-require 'capybara'
-require 'selenium-webdriver'
+require 'capybara/dsl'
+require 'capybara-webkit'
 require 'dotenv'
+require 'orchestrate'
 
 Dotenv.load
 
-include Capybara::DSL
+Capybara.default_driver = :webkit
+Capybara.default_max_wait_time = 20
+Capybara::Webkit.configure do |config|
+  config.allow_url('intelligent-tickets.co.uk')
+  config.allow_url("www.paypal.com")
+end
 
-Capybara.default_driver = :selenium
+class Checker
+  include Capybara::DSL
 
-visit 'http://intelligent-tickets.co.uk/index.php?th=tw&pg=login'
+  def run
+    visit 'http://intelligent-tickets.co.uk/index.php?th=tw&pg=login'
 
-fill_in 'user', with: ENV['USER']
-fill_in 'pass', with: ENV['PASSWORD']
-click_button 'LOG IN'
+    fill_in 'user', with: ENV['USER']
+    fill_in 'pass', with: ENV['PASSWORD']
+    check 'cookiecheck'
+    click_button 'LOG IN'
+
+    within '#messagediv' do
+      click_button 'Close message'
+    end
+
+    within '#date_table' do
+      find('tr.tabletext').click
+    end
+
+    within 'form#book' do
+      click_button 'Check availability'
+
+      if page.has_content?('Seats available online')
+        puts 'SYSTEM IS UP'
+        return :up
+      else
+        puts 'SYSTEM IS DOWN'
+        return :down
+      end
+    end
+  end
+end
+
+app = Orchestrate::Application.new(ENV['ORCHESTRATE_API_KEY'])
+uptime = app[:uptime]
+record = uptime[ENV['ORCHESTRATE_CHECKER_ID']]
+
+status = Checker.new.run
+record.events['status_check'] << { status: status.to_s }
