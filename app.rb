80b49264a7741dc76481714086db04d511ff816f
Ben Forrest <ben@silvabox.com>
2016-10-15 21:08:53 +0100

    wires index to last seven days data

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
+require './lib/uptime'
 
 get '/' do
   haml :index
 end
 
+get '/api/last_seven_days' do
+  chart_json(Uptime.last_seven_days.by_day.percentages)
+end
+
+
+def chart_json(uptime)
+  {
+    labels: uptime.keys,
+    data: uptime.values
+  }.to_json
+end
